6ca7f5df5b7aa86aa51b04ab955e6087511e090c
Banee Ishaque K <baneeishaque@gmail.com>
2021-06-26 08:28:47 +0530

    U9 & For.

--- common11/src/main/java/ndk/utils_android11/DatePickerUtils.java
+++ common11/src/main/java/ndk/utils_android11/DatePickerUtils.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import java.util.Calendar;
 import java.util.Date;
 
-import ndk.utils_android1.DateUtils;
+import ndk.utils_android1.DateUtils1;
 
 public class DatePickerUtils {
 
@@ -32,7 +32,7 @@ public class DatePickerUtils {
 
     public static void showDatePickerUpToTomorrow(Context context, DatePickerDialog.OnDateSetListener date_chooser_listener, Calendar calendar) {
         DatePickerDialog date_Picker_Dialog = new DatePickerDialog(context, date_chooser_listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils.addDays(new Date(), 1).getTime());
+        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils1.addDays(new Date(), 1).getTime());
         date_Picker_Dialog.show();
 
     }
@@ -47,7 +47,7 @@ public class DatePickerUtils {
 
     public static void showDatePickerFromSelectedDateToTomorrow(Context context, DatePickerDialog.OnDateSetListener date_chooser_listener, Calendar calendar) {
         DatePickerDialog date_Picker_Dialog = new DatePickerDialog(context, date_chooser_listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils.addDays(new Date(), 1).getTime());
+        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils1.addDays(new Date(), 1).getTime());
         date_Picker_Dialog.getDatePicker().setMinDate(calendar.getTime().getTime());
         date_Picker_Dialog.show();
 
@@ -55,9 +55,9 @@ public class DatePickerUtils {
 
     public static String getTitleFromMysqlDateStrings(String operation, String start_date, String end_date, String APPLICATION_NAME) {
         if (start_date.equals(end_date)) {
-            return operation + " : " + DateUtils.mysqlDateStringToNormalDateString(start_date, APPLICATION_NAME);
+            return operation + " : " + DateUtils1.mysqlDateStringToNormalDateString(start_date, APPLICATION_NAME);
         } else {
-            return operation + " : " + DateUtils.mysqlDateStringToNormalDateString(start_date, APPLICATION_NAME) + " - " + DateUtils.mysqlDateStringToNormalDateString(end_date, APPLICATION_NAME);
+            return operation + " : " + DateUtils1.mysqlDateStringToNormalDateString(start_date, APPLICATION_NAME) + " - " + DateUtils1.mysqlDateStringToNormalDateString(end_date, APPLICATION_NAME);
         }
     }
 
@@ -65,14 +65,14 @@ public class DatePickerUtils {
 
         Calendar calendar = Calendar.getInstance();
 
-        Log.d(APPLICATION_NAME, "Date : " + DateUtils.dateToNormalDateString(calendar.getTime()));
+        Log.d(APPLICATION_NAME, "Date : " + DateUtils1.dateToNormalDateString(calendar.getTime()));
         Log.d(APPLICATION_NAME, "Year : " + calendar.get(Calendar.YEAR));
         Log.d(APPLICATION_NAME, "Month : " + calendar.get(Calendar.MONTH));
         Log.d(APPLICATION_NAME, "Day : " + calendar.get(Calendar.DAY_OF_MONTH));
 
         DatePickerDialog date_Picker_Dialog = new DatePickerDialog(context, date_chooser_listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
-        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils.addDays(new Date(), 2).getTime());
+        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils1.addDays(new Date(), 2).getTime());
 
         date_Picker_Dialog.show();
     }
@@ -81,12 +81,12 @@ public class DatePickerUtils {
 
         DatePickerDialog date_Picker_Dialog = new DatePickerDialog(context, date_chooser_listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
-        Log.d(APPLICATION_NAME, "Date : " + DateUtils.dateToNormalDateString(calendar.getTime()));
+        Log.d(APPLICATION_NAME, "Date : " + DateUtils1.dateToNormalDateString(calendar.getTime()));
         Log.d(APPLICATION_NAME, "Year : " + calendar.get(Calendar.YEAR));
         Log.d(APPLICATION_NAME, "Month : " + calendar.get(Calendar.MONTH));
         Log.d(APPLICATION_NAME, "Day : " + calendar.get(Calendar.DAY_OF_MONTH));
 
-        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils.addDays(new Date(), 2).getTime());
+        date_Picker_Dialog.getDatePicker().setMaxDate(DateUtils1.addDays(new Date(), 2).getTime());
 
         date_Picker_Dialog.getDatePicker().setMinDate(calendar.getTime().getTime());
 
